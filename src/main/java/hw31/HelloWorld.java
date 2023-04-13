fa7c71e6323457715d8082c7069fe5fd6471939d
Serhii Kovalenko <117315121+sergeyko2@users.noreply.github.com>
2023-04-13 17:41:47 +0300

    Revert "Final commit" (#2) (#3)

--- src/main/java/hw31/HelloWorld.java
+++ src/main/java/hw31/HelloWorld.java
@@ -1,13 +1,4 @@
 package hw31;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-
-@Path("")
 public class HelloWorld {
-
-    @GET
-    public String index() {
-        return "Hello, World!";
-    }
 }
